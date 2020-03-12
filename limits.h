a62d85baaef2b98aaa685b12bb1a35bbfa602670
Giacomo Bighin <bighin@gmail.com>
2020-03-12 20:28:23 +0100

    Added missing file limits.h

---
+++ limits.h
@@ -0,0 +1,19 @@
+#ifndef PMATRIX_MAX_DIMENSIONS
+#define PMATRIX_MAX_DIMENSIONS	(64)
+#endif
+
+#ifndef MAX_LABELS
+#define MAX_LABELS		(32)
+#endif
+
+#ifndef MAX_MATRIX_ELEMENTS
+#define MAX_MATRIX_ELEMENTS	(16)
+#endif
+
+#ifndef MAX_DENOMINATORS
+#define MAX_DENOMINATORS	(16)
+#endif
+
+#ifndef MAX_NUMERATORS
+#define MAX_NUMERATORS	(16)
+#endif
